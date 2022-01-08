5b19e660f1968e2caacb8f18a5c3f636df691505
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-01-08 21:39:31 +0100

    fix(zsh): fix aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,6 +1,3 @@
-# Enable aliases to be sudo'ed
-alias sudo="sudo"
-
 # NAVIGATION
 alias ..="cd ../"
 alias ...="cd ../../"
@@ -16,17 +13,17 @@ alias cdr="cd ~/Repos"
 
 # LISTING
 alias ls='lsd'
-alias l="ls -l"
-alias la="ls -la"
-alias lt="ls -lt"
-alias lat="ls -lat"
-alias lrt="ls -lrt"
-alias lart="ls -lart"
-alias lS="ls -lS"
-alias las="ls -laS"
-alias lrs="ls -lrS"
-alias lars="ls -larS"
-alias ldot="ls -ld .*"
+alias l="lsd -l"
+alias la="lsd -la"
+alias lt="lsd -lt"
+alias lat="lsd -lat"
+alias lrt="lsd -lrt"
+alias lart="lsd -lart"
+alias lS="lsd -lS"
+alias las="lsd -laS"
+alias lrs="lsd -lrS"
+alias lars="lsd -larS"
+alias ldot="lsd -ld .*"
 alias tree="lsd --tree"
 
 # FILES
@@ -50,3 +47,6 @@ alias afk="osascript -e 'tell app \"System Events\" to key code 12 using {contro
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
 alias update="sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup"
+
+# VIM
+alias vim="nvim"
