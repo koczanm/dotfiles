ffcf09deaa6cecd8c8939e304cf5d982e5a03bea
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-04-13 20:31:48 +0200

    fix(nvim): fix pyright

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -4,25 +4,15 @@ M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
    local utils = require "custom.plugins.utils"
 
-   lspconfig.tsserver.setup {
-      on_attach = function(client, bufnr)
-         client.resolved_capabilities.document_formatting = false
-         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-      end,
-   }
-
    lspconfig.pyright.setup {
       before_init = function(_, config)
           config.setting.python.pythonPath = utils.get_python_path(config.root_dir)
-      end
-      on_attach = function(client, bufnr)
-         client.resolved_capabilities.document_formatting = false
-         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-      end
+      end,
+      on_attach = attach,
    }
 
    -- lspservers with default config
-   local servers = { "bashls" }
+   local servers = { "bashls", "tsserver" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
