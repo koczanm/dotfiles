311f5f1a84a28607ca6e46792caf608d8d520578
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-05-22 19:20:39 +0200

    refactor(install): use system-aware install bin

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ mkdir -p "${HOME}/.dotfiles/"
 curl -fsSL https://github.com/koczanm/dotfiles/tarball/main | tar -xzv --strip-components=1 -C "${HOME}/.dotfiles"
 
 # Run bootstrap scripts
-${HOME}/.dotfiles/bin/bootstrap-apps
-${HOME}/.dotfiles/bin/bootstrap-dotfiles
-${HOME}/.dotfiles/bin/bootstrap-macos
+case "${OSTYPE}" in
+    linux*) 
+        ${HOME}/.dotfiles/bin/bootstrap-apps-linux
+        ${HOME}/.dotfiles/bin/bootstrap-dotfiles
+        ;;
+    darwin*)
+        ${HOME}/.dotfiles/bin/bootstrap-apps-macos
+        ${HOME}/.dotfiles/bin/bootstrap-dotfiles
+        ${HOME}/.dotfiles/bin/bootstrap-macos
+        ;;
+    *)
+        echo "Unknown OS: ${OSTYPE}"
+        exit 1
+esac
