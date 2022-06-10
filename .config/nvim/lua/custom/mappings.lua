27083ba4f171a1021aa260c42b503dc968b098aa
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-06-10 08:50:49 +0200

    fix(nvim): fix mappings

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -30,23 +30,23 @@ M.general = {
       },
 
       -- resize windows
-      ["<leader>w="] = { "<C-w>=", "   realign"},
-      ["<leader>wh"] = { "<C-w><", "ﲕ   expand left"},
-      ["<leader>wl"] = { "<C-w>>", "ﲖ   expand right"},
-      ["<leader>wk"] = { "<C-w>+", "ﲗ   expand up"},
-      ["<leader>wj"] = { "<C-w>-", "ﲔ   expand down"},
+      ["<leader>w="] = { "<C-w>=", "  realign"},
+      ["<leader>wh"] = { "<C-w><", "ﲕ  expand left"},
+      ["<leader>wl"] = { "<C-w>>", "ﲖ  expand right"},
+      ["<leader>wk"] = { "<C-w>+", "ﲗ  expand up"},
+      ["<leader>wj"] = { "<C-w>-", "ﲔ  expand down"},
 
       -- split windows
-      ["<leader>ws"] = { "<C-w>s", "ﰮ   new horizontal window"},
-      ["<leader>wv"] = { "<C-w>v", "ﰴ   new vertical window"},
+      ["<leader>ws"] = { "<C-w>s", "ﰮ  new horizontal window"},
+      ["<leader>wv"] = { "<C-w>v", "ﰴ  new vertical window"},
 
       -- close windows
-      ["<leader>wq"] = { "<cmd> quit <CR>", "   close window"},
-      ["<leader>wQ"] = { "<cmd> quit! <CR>", "   force close window"},
+      ["<leader>wq"] = { "<cmd> quit <CR>", "  close window"},
+      ["<leader>wQ"] = { "<cmd> quit! <CR>", "  force close window"},
 
       -- quit nvim
-      ["<leader>qq"] = { "<cmd> quitall <CR>", "   quit" },
-      ["<leader>QQ"] = { "<cmd> quitall! <CR>", "   force quit" },
+      ["<leader>qq"] = { "<cmd> quitall <CR>", "  quit" },
+      ["<leader>QQ"] = { "<cmd> quitall! <CR>", "  force quit" },
    },
 }
 
@@ -57,22 +57,27 @@ M.lspconfig = {
          function()
             vim.lsp.buf.add_workspace_folder()
          end,
-         "   add workspace folder",
+         "  add workspace folder",
       },
 
       ["<leader>wsr"] = {
          function()
             vim.lsp.buf.remove_workspace_folder()
          end,
-         "   remove workspace folder",
+         "  remove workspace folder",
       },
 
       ["<leader>wsl"] = {
          function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
          end,
-         "   list workspace folders",
+         "  list workspace folders",
       },
+
+      -- disable
+      ["<leader>wK"] = "",
+      ["<leader>wa"] = "",
+      ["<leader>wr"] = "",
    },
 }
 
@@ -92,13 +97,16 @@ M.nvterm = {
          end,
          "   new vertical term",
       },
+
+      ["<leader>h"] = "",
+      ["<leader>v"] = "",
    },
 }
 
 M.outline = {
 
    n = {
-      ["<leader>ls"] = { "<cmd> SymbolsOutline <CR>", "   outline view" },
+      ["<leader>ls"] = { "<cmd> SymbolsOutline <CR>", "  outline view" },
    },
 }
 
@@ -106,48 +114,34 @@ M.telescope = {
 
    n = {
       -- find
-      ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "   live grep"},
+      ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "  find files" },
+      ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "  find all" },
+      ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "  live grep"},
+      ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "  find buffers" },
+      ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "  help page" },
+      ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "   find oldfiles" },
       ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "   show keys" },
 
       -- git
-      ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "שׂ   git commits" },
-      ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "   git commits" },
+      ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "שׂ  git commits" },
+      ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "  git commits" },
       ["<leader>gst"] = { "<cmd> Telescope git_status <CR>", "  git status" },
 
       -- pick a hidden term
-      ["<leader>ft"] = { "<cmd> Telescope terms <CR>", "   pick hidden term" },
+      ["<leader>ft"] = { "<cmd> Telescope terms <CR>", "  pick hidden term" },
+
+      -- disable
+      ["<leader>cm"] = "",
+      ["<leader>gt"] = "",
+      ["<leader>pt"] = "",
+      ["<leader>tk"] = "",
    },
 }
 
 M.tld = {
 
    n = {
-      ["<leader>td"] = { "<cmd> ToggleDiag <CR>", "𥳐   diagnostics" },
-   },
-}
-
-M.disabled = {
-
-   n = {
-      ["<leader>cm"] = "",
-      ["<leader>fw"] = "",
-      ["<leader>gt"] = "",
-      ["<leader>pt"] = "",
-      ["<leader>tk"] = "",
-      ["<leader>wK"] = "",
-      ["<leader>wa"] = "",
-      ["<leader>wr"] = "",
-      ["<leader>h"] = "",
-      ["<leader>v"] = "",
-      ["<A-i>"] = "",
-      ["<A-h>"] = "",
-      ["<A-v>"] = "",
-   },
-
-   t = {
-      ["<A-i>"] = "",
-      ["<A-h>"] = "",
-      ["<A-v>"] = "",
+      ["<leader>td"] = { "<cmd> ToggleDiag <CR>", "𥳐  diagnostics" },
    },
 }
 
