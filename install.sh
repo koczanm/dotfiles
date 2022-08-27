4ad3d6e0a9665f3d541b14e68e17fdfd0bfcba56
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-08-27 17:57:19 +0200

    fix(install): download zero.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 DOTFILES_DIR="${HOME}/.dotfiles"
 
 # Create dotfiles directory
-mkdir -p ${DOTFILES_DIR}
+mkdir -p "${DOTFILES_DIR}"
 
-# Download tarball
-curl -fsSL https://github.com/koczanm/dotfiles/tarball/main | tar -xzv --strip-components=1 -C ${DOTFILES_DIR}
+# Download dotfiles
+curl -fsSL https://github.com/koczanm/dotfiles/tarball/main | tar -xzv --strip-components=1 -C "${DOTFILES_DIR}"
+
+# Download zero.sh
+curl -fsSL https://github.com/zero-sh/zero.sh/tarball/master | tar -xzv --strip-components=1 -C "${DOTFILES_DIR}/zero"
 
 # Run bootstrap scripts
 ${DOTFILES_DIR}/zero/setup
