27b07578f1a4270e185feb24cd7d9450f945a9a8
Mateusz Koczan <xmateuszkoczan@gmail.com>
2022-09-06 20:38:38 +0200

    fix(macos): fix system prefs location

--- run/after/9_macos.sh
+++ run/after/9_macos.sh
@@ -3,9 +3,9 @@
 echo "Updating Dock..."
 
 dockutil --remove all --no-restart
-dockutil --add "/Applications/Mail.app" --no-restart
+dockutil --add "/System/Applications/Mail.app" --no-restart
 dockutil --add "/Applications/Safari.app" --no-restart
 dockutil --add "/Applications/Spotify.app" --no-restart
 dockutil --add "/Applications/iTerm.app" --no-restart
-dockutil --add "/Applications/System Preferences.app" --no-restart
+dockutil --add "/System/Applications/System Preferences.app" --no-restart
 dockutil --add "${HOME}/Desktop/Screenshots" --display stack  # Implicitly restarts the Dock
