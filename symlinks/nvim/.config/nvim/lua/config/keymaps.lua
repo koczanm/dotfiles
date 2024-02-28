9d7b5e886f14ef5785b25e972f34f65ccc24f31c
Mateusz Koczan <xmateuszkoczan@gmail.com>
2024-02-28 21:17:13 +0100

    chore(tmux): improve keymaps

--- symlinks/nvim/.config/nvim/lua/config/keymaps.lua
+++ symlinks/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move to window using the <ctrl> <shift> hjkl keys
+vim.keymap.set("n", "<M-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<M-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<M-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<M-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- Move to window using the <option> hjkl keys
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
--- symlinks/nvim/.config/nvim/lua/plugins/ui.lua
+++ symlinks/nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,5 @@
 return {
-  -- configure catppuccin
+  -- use latte & frappe catppuccin flavours
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -12,7 +12,7 @@ return {
     },
   },
 
-  -- configure LazyVim to load catppuccin
+  -- use catppuccin as default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
@@ -20,7 +20,7 @@ return {
     },
   },
 
-  --
+  -- automatically switch between light/dark mode
   {
     "f-person/auto-dark-mode.nvim",
     config = {
@@ -34,7 +34,7 @@ return {
     },
   },
 
-  --
+  -- scroll smoothly
   {
     "karb94/neoscroll.nvim",
     event = "VeryLazy",
@@ -44,7 +44,7 @@ return {
     end,
   },
 
-  --
+  -- don't set indent scope for leetcode.nvim
   {
     "echasnovski/mini.indentscope",
     init = function()
@@ -68,5 +68,49 @@ return {
         end,
       })
     end,
+  },
+
+  -- switch nvim windows with awareness of tmux panes
+  {
+    "aserowy/tmux.nvim",
+    keys = {
+      {
+        "<M-h>",
+        function()
+          require("tmux").move_left()
+        end,
+        desc = "Move to left window",
+      },
+      {
+        "<M-j>",
+        function()
+          require("tmux").move_bottom()
+        end,
+        desc = "Move to lower window",
+      },
+      {
+        "<M-k>",
+        function()
+          require("tmux").move_top()
+        end,
+        desc = "Move to upper window",
+      },
+      {
+        "<M-l>",
+        function()
+          require("tmux").move_right()
+        end,
+        desc = "Move to right window",
+      },
+    },
+    opts = {
+      navigation = {
+        enable_default_keybindings = false,
+      },
+      resize = {
+
+        enable_default_keybindings = false,
+      },
+    },
   },
 }
